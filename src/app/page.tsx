e228e3a7335e680054e06fde13350077993ca8e1
caru-ini <s1F102400392@iniad.org>
2024-05-02 23:45:44 +0900

    Refactor Reversi game logic and UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Game from "@/components/reversi/game";
 
 export default function Home() {
-    return (
-        <main className="min-h-screen flex flex-col justify-center items-center">
-            {/* Reversi UI */}
-            <div className="flex items-center justify-center gap-10 w-[90vmin]">
-                <Game/>
-                {/* Reversi Game */}
-            </div>
-        </main>
-    );
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      {/* Reversi UI */}
+      <div className="flex items-center justify-center gap-10 w-[90vmin]">
+        <Game />
+        {/* Reversi Game */}
+      </div>
+    </main>
+  );
 }
